44b612eafae2ec808ecd70c3532a298f59aa363e
rakybalint <rakybalint@gmail.com>
2023-11-06 14:14:33 +0100

    tesla feladat

---
+++ forras/data.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Model\Tesla;
+
+$cars = [
+    new Tesla("Model S Performance",    2.5,    639,    5,  "model-s.png"),
+    new Tesla("Model S Long Range Plus",3.8,    652,    5,  "model-s.png"),
+    new Tesla("Model 3 Performance",    3.3,    567,    5,  "model-3.png"),
+    new Tesla("Model 3 Long Range",     4.4,    580,    5,  "model-3.png"),
+    new Tesla("Model 3 Standard Plus",  5.6,    430,    5,  "model-3.png"),
+    new Tesla("Model X Performance",    2.8,    548,    7,  "model-x.png"),
+    new Tesla("Model X Long Range",     4.6,    561,    7,  "model-x.png"),
+    new Tesla("Model Y Performance",    3.7,    480,    7,  "model-y.png"),
+    new Tesla("Model Y Long Range AWD", 5.1,    505,    7,  "model-y.png"),
+];
